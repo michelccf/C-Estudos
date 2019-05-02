9cb99902a67629b70d4db570438ba074b768b078
mcanesin <mcanesin@brq.com>
2019-05-02 02:03:54 -0300

    Primeiro commit

---
+++ C++/Avançado/Alocação_dinâmica_memoria/Classe_Listas.h
@@ -0,0 +1,103 @@
+#include <iostream>;
+
+class No
+{
+
+public: int v;
+static No *proxNo;
+static No *antNo;
+
+public: No()
+{
+	this->proxNo = NULL;
+	this->antNo = NULL;
+}
+
+public: int retornarValor()
+{
+	return v;
+}
+
+public: void editarValor(int valor)
+{
+	this->v = valor;
+}
+
+public: No * proximoNo()
+{
+	return this->proxNo;
+}
+
+public: No * anteriorNo()
+{
+	return this->proxNo;
+}
+
+public: void liberarMemNo()
+{
+	free(this);
+}
+
+
+};
+
+class Lista_s_cl
+{
+	No* FirstOrDefaut;
+	No* LastOrDefault;
+
+public: Lista_s_cl()
+{
+	FirstOrDefaut = NULL;
+	LastOrDefault = NULL;
+}
+
+public: void Add(int valor)
+{
+	No* novo = static_cast<No*>(malloc(sizeof(No)));
+	novo->v = valor;
+
+	novo->antNo = LastOrDefault;
+	novo->proxNo = NULL;
+
+	if (LastOrDefault == NULL) {
+		FirstOrDefaut = novo;
+		LastOrDefault = novo;
+	}
+	else {
+		LastOrDefault->proxNo = novo;
+		LastOrDefault = LastOrDefault->proxNo;
+	}
+}
+
+public: int Count()
+{
+	No* iterador = FirstOrDefaut;
+	int count = 0;
+	if (FirstOrDefaut == NULL)
+		return 0;
+	else
+		do 
+		{
+			count++;
+			iterador = iterador->proxNo;
+		} while (iterador != LastOrDefault);
+		return count;
+}
+
+public: void ApagarLista()
+{
+	No* iterador = FirstOrDefaut;
+	No* tmp = FirstOrDefaut;
+	if (FirstOrDefaut == NULL)
+		return;
+	else
+		do
+		{
+			tmp = iterador;
+			iterador = iterador->proxNo;
+			free(tmp);
+		} while (iterador != LastOrDefault);
+}
+
+};
---
+++ C++/Avançado/Alocação_dinâmica_memoria/Structs.h
@@ -0,0 +1,12 @@
+struct lista_s
+{
+	int v;
+	static lista_s *proxNo;
+};
+
+struct lista_d
+{
+	int v;
+	static lista_d *proxNo;
+	static lista_d *antNo;
+};
